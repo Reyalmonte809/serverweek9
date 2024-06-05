9bdf466a91bbcf13c73143eb90e6df4c1910fc45
reyalmonte809 <reyalmonte89@gmail.com>
2024-06-05 15:41:05 -0400

    thank you kevin

--- server.js
+++ server.js
@@ -1,10 +1,55 @@
 const http = require('http')
 
 function handleRequest(request, response){
-    response.write(`Your URL was: ` );//Build it
-    response.write(request.url) /// build it more
-    return response.end(); //// send it
+    if("GET" === request.method && 
+       "/product/" === request.url){
+        
+        response.setHeader("Content-Type", "text/json");
+
+        response.setHeader(`Access-Control-Allow-Origin`, `*`); // * WILDCARD
+        response.setHeader(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
+        response.setHeader(`Access-Control-Allow-Headers`, `Content-Type`);
+
+        response.write(`
+        [
+            {
+                "name": "Pencil",
+                "desc": "Wood lead pencil #2 with eraser.",
+                "price": 0.25,
+                "images": [
+                    {
+                        "name" : "default",
+                        "path" : "./images/pencil.png"
+                    },
+                    {
+                        "name" : "side",
+                        "path" : "./images/pencil_side.png"
+                    }
+                ]
+            },
+            {
+                "name": "Paper",
+                "desc": "8.5x11\\\" recycled stock.",
+                "price": 2.50,
+                "images": [
+                    {
+                        "name" : "default",
+                        "path" : "./images/paper.png"
+                    },
+                    {
+                        "name" : "top",
+                        "path" : "./images/paper_top.png"
+                    }
+                ]
+            }
+        ]
+        `);
+    }else{
+        response.write(`Your url was: `); //BUILD IT
+        response.write(request.url); //BUILD IT MORE
+    }
+    return response.end(); //SEND IT
 }
 
-const server = http.createServer(handleRequest)
-server.listen(9090)
+const server = http.createServer(handleRequest);
+server.listen(9090);
